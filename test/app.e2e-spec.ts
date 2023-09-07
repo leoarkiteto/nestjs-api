50bfdafe58cdb95d4904209e1bdfe5d6526eb616
Leo Franca <leoarkiteto@gmail.com>
2023-09-07 11:33:21 +0100

    :heavy_plus_sign: Setting e2e environment

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,24 +1,62 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { INestApplication } from '@nestjs/common';
-import * as request from 'supertest';
-import { AppModule } from '@/app.module';
+import { Test } from "@nestjs/testing";
+import { INestApplication, ValidationPipe } from "@nestjs/common";
+import * as pactum from "pactum";
 
-describe('AppController (e2e)', () => {
+import { AppModule } from "@/app.module";
+import { PrismaService } from "@/prisma/prisma.service";
+import { AuthDto } from "@/auth/dto";
+
+describe("App e2e", () => {
   let app: INestApplication;
-
-  beforeEach(async () => {
-    const moduleFixture: TestingModule = await Test.createTestingModule({
+  let prisma: PrismaService;
+  beforeAll(async () => {
+    const moduleRef = await Test.createTestingModule({
       imports: [AppModule],
     }).compile();
 
-    app = moduleFixture.createNestApplication();
+    app = moduleRef.createNestApplication();
+    app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
     await app.init();
+    await app.listen(3000);
+
+    prisma = app.get(PrismaService);
+    await prisma.cleanDb();
+    pactum.request.setBaseUrl("http://localhost:3000");
   });
 
-  it('/ (GET)', () => {
-    return request(app.getHttpServer())
-      .get('/')
-      .expect(200)
-      .expect('Hello World!');
+  afterAll(() => {
+    app.close();
+  });
+
+  describe("Auth", () => {
+    describe("Signup", () => {
+      it("should signup", () => {
+        const dto: AuthDto = {
+          email: "lucas@email.com",
+          password: "123",
+        };
+        return pactum
+          .spec()
+          .post("/auth/signup")
+          .withBody(dto)
+          .expectStatus(201);
+      });
+    });
+    describe("Signin", () => {
+      it.todo("should signin");
+    });
+  });
+
+  describe("User", () => {
+    describe("Get me", () => {});
+    describe("Edit user", () => {});
+  });
+
+  describe("Bookmark", () => {
+    describe("Get bookmark", () => {});
+    describe("Get by id bookmark", () => {});
+    describe("Create bookmark", () => {});
+    describe("Edit bookmark", () => {});
+    describe("Delete bookmark", () => {});
   });
 });
